54a039791b12a07046fab5bb3f1bcb984129d8fc
Simon Thiesen <simon.thiesen@online.de>
2021-11-21 16:33:47 +0100

    changed all_cells to Grid

--- src/grid_bound.rs
+++ src/grid_bound.rs
@@ -1,19 +1,20 @@
+use super::grid::*;
 use super::util::*;
 use glam::DVec2 as Vec2;
 use glam::IVec2;
 use std::collections::HashSet;
 // use coz::*;
 
-const GRID_SIZE: usize = 100000;
-const SIDE_LENGTH: f64 = 4. / GRID_SIZE as f64;
+pub const GRID_SIZE: usize = 500;
+pub const SIDE_LENGTH: f64 = 4. / GRID_SIZE as f64;
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy)]
-struct Cell {
+pub struct Cell {
     center: IVec2,
 }
 
 impl Cell {
-    fn new(center: IVec2) -> Self {
+    pub fn new(center: IVec2) -> Self {
         Cell { center }
     }
     fn get_center(&self) -> Vec2 {
@@ -26,7 +27,8 @@ impl Cell {
     fn in_set(&self, limit: usize, rng: &mut ThreadRng) -> bool {
         // is_inside(&self.gen_point_inside(rng), limit)
         let point = self.gen_point_inside(rng);
-        is_inside(&point, limit) && !is_any_cycle(point)
+        // is_inside(&point, limit) && !is_any_cycle(point)
+        is_inside(&point, limit) && !is_inside(&point, limit * 10)
     }
     fn get_neighbors(&self) -> Vec<Cell> {
         vec![
@@ -42,22 +44,26 @@ impl Cell {
     fn area() -> f64 {
         SIDE_LENGTH * SIDE_LENGTH
     }
+    pub fn index(&self) -> (usize, usize) {
+        let index = self.center + IVec2::splat((GRID_SIZE / 2) as i32);
+        (index.x as usize, index.y as usize)
+    }
 }
 
-pub struct Grid {
-    inside_cells: HashSet<Cell>,
+pub struct CovarageGrid {
+    inside_cells: Grid,
     neighbors: HashSet<Cell>,
-    all_cells: HashSet<Cell>,
+    all_cells: Grid,
     new_neighbors: HashSet<Cell>,
     limit: usize,
 }
 
-impl Grid {
+impl CovarageGrid {
     pub fn new(limit: usize) -> Self {
-        let mut grid = Grid {
-            inside_cells: HashSet::new(),
+        let mut grid = CovarageGrid {
+            inside_cells: Grid::new(),
             neighbors: HashSet::new(),
-            all_cells: HashSet::new(),
+            all_cells: Grid::new(),
             new_neighbors: HashSet::new(),
             limit,
         };
@@ -65,7 +71,9 @@ impl Grid {
         grid.inside_cells.insert(start);
         grid.neighbors.extend(start.get_neighbors());
         grid.all_cells.insert(start);
-        grid.all_cells.extend(grid.neighbors.iter().cloned());
+        for cell in grid.neighbors.iter() {
+            grid.all_cells.insert(*cell);
+        }
         grid.new_neighbors.extend(grid.neighbors.iter().cloned());
         grid
     }
@@ -85,9 +93,11 @@ impl Grid {
         let new_neighbors_copy = self.new_neighbors.clone();
         self.new_neighbors.clear();
         for cell in new_neighbors_copy {
-            if cell.in_set(self.limit, rng) {
-                self.add_inside_cell(cell);
-                continue;
+            for _ in 0..100 {
+                if cell.in_set(self.limit, rng) {
+                    self.add_inside_cell(cell);
+                    break;
+                }
             }
         }
         self.neighbors.extend(self.new_neighbors.iter().cloned());
@@ -96,25 +106,23 @@ impl Grid {
         self.inside_cells.insert(cell);
         self.neighbors.remove(&cell);
         for neighbor in cell.get_neighbors() {
-            if !self.all_cells.contains(&neighbor) {
+            if !self.all_cells.is_activ(neighbor) {
                 self.new_neighbors.insert(neighbor);
                 self.all_cells.insert(neighbor);
             }
         }
     }
     pub fn draw(&self) {
-        // for inside in self.inside_cells.iter() {
-        //     inside.draw(GREEN);
-        // }
+        self.inside_cells.draw();
         // for neighbors in self.neighbors.iter() {
         //     neighbors.draw(RED);
         // }
         for new_neighbors in self.new_neighbors.iter() {
-            new_neighbors.draw(RED);
+            new_neighbors.draw(BLUE);
         }
     }
     pub fn area(&self) -> f64 {
-        self.inside_cells.len() as f64 * Cell::area()
+        self.inside_cells.activ_count() as f64 * Cell::area()
     }
     pub fn new_neighbor_len(&self) -> usize {
         self.new_neighbors.len()
