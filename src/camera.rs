84a57a5539b85b7006a56fdafdd081fd3369496f
Simon Thiesen <simon.thiesen@online.de>
2021-11-25 21:39:03 +0100

    cleaned up zoom

--- src/camera.rs
+++ src/camera.rs
@@ -23,21 +23,28 @@ impl CameraManger {
         set_camera(&Camera2D::from_display_rect(manger.camera_rect));
         manger
     }
+    fn zoom(&mut self) {
+        let inverse_zoom_factor = 1. / self.zoom_factor;
+        let mouse_screen_pos = mouse_position_local();
+
+        let camera_corner = Vec2::new(self.camera_rect.x, self.camera_rect.y);
+        let camera_size = Vec2::new(self.camera_rect.w, self.camera_rect.h);
+
+        let camera_offset = mouse_screen_pos * camera_size;
+
+        let center_offset = camera_size * 0.25;
+        let new_camera_corner = camera_corner + camera_offset * 0.25 + center_offset;
+
+        let new_h = inverse_zoom_factor * self.camera_rect.h;
+        let new_w = inverse_zoom_factor * self.camera_rect.w;
+        self.camera_rect = Rect::new(new_camera_corner.x, new_camera_corner.y, new_w, new_h);
+        self.zoom *= self.zoom_factor;
+        self.line_width = self.starting_line_width / self.zoom;
+        set_camera(&Camera2D::from_display_rect(self.camera_rect));
+    }
     pub fn update(&mut self) {
         if is_mouse_button_pressed(MouseButton::Left) {
-            let inverse_zoom_factor = 1. - 1. / self.zoom_factor;
-            let mouse_pos = mouse_position_local();
-            let positiv_mouse_pos = 0.5 * (mouse_pos + Vec2::new(1., 1.));
-            let new_x =
-                self.camera_rect.x + self.camera_rect.w * inverse_zoom_factor * positiv_mouse_pos.x;
-            let new_y =
-                self.camera_rect.y + self.camera_rect.h * inverse_zoom_factor * positiv_mouse_pos.y;
-            let new_h = inverse_zoom_factor * self.camera_rect.h;
-            let new_w = inverse_zoom_factor * self.camera_rect.w;
-            self.camera_rect = Rect::new(new_x, new_y, new_w, new_h);
-            self.zoom *= self.zoom_factor;
-            self.line_width = self.starting_line_width / self.zoom;
-            set_camera(&Camera2D::from_display_rect(self.camera_rect));
+            self.zoom();
         }
         if is_key_pressed(KeyCode::Space) {
             self.camera_rect = Rect::new(-2.01, -1.26, 3.02, 2.52);
