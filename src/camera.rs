ce83e615989a698d375b20e94458266d8bb09029
Simon Thiesen <simon.thiesen@online.de>
2021-11-25 21:55:47 +0100

    improfed zoom to work with all values instead of only 2

--- src/camera.rs
+++ src/camera.rs
@@ -23,28 +23,31 @@ impl CameraManger {
         set_camera(&Camera2D::from_display_rect(manger.camera_rect));
         manger
     }
-    fn zoom(&mut self) {
-        let inverse_zoom_factor = 1. / self.zoom_factor;
+    fn zoom(&mut self, zoom: f32) {
         let mouse_screen_pos = mouse_position_local();
 
         let camera_corner = Vec2::new(self.camera_rect.x, self.camera_rect.y);
         let camera_size = Vec2::new(self.camera_rect.w, self.camera_rect.h);
 
         let camera_offset = mouse_screen_pos * camera_size;
+        let camara_rect_shrinkage = 0.5 * (1. - 1. / zoom);
+        let center_offset = camera_size * camara_rect_shrinkage;
+        let new_camera_corner =
+            camera_corner + camera_offset * camara_rect_shrinkage + center_offset;
 
-        let center_offset = camera_size * 0.25;
-        let new_camera_corner = camera_corner + camera_offset * 0.25 + center_offset;
-
-        let new_h = inverse_zoom_factor * self.camera_rect.h;
-        let new_w = inverse_zoom_factor * self.camera_rect.w;
+        let new_h = self.camera_rect.h / zoom;
+        let new_w = self.camera_rect.w / zoom;
         self.camera_rect = Rect::new(new_camera_corner.x, new_camera_corner.y, new_w, new_h);
-        self.zoom *= self.zoom_factor;
+        self.zoom *= zoom;
         self.line_width = self.starting_line_width / self.zoom;
         set_camera(&Camera2D::from_display_rect(self.camera_rect));
     }
     pub fn update(&mut self) {
         if is_mouse_button_pressed(MouseButton::Left) {
-            self.zoom();
+            self.zoom(self.zoom_factor);
+        }
+        if is_mouse_button_pressed(MouseButton::Right) {
+            self.zoom(1. / self.zoom_factor);
         }
         if is_key_pressed(KeyCode::Space) {
             self.camera_rect = Rect::new(-2.01, -1.26, 3.02, 2.52);
