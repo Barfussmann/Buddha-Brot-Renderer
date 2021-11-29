48fd3d28c6971824d21c18492a4c991ef8e0751e
Simon Thiesen <simon.thiesen@online.de>
2021-11-29 16:52:55 +0100

    changed back to f64x8

--- src/mandel_brot_render.rs
+++ src/mandel_brot_render.rs
@@ -9,7 +9,7 @@ pub struct MandelbrotRender {
     top_left_corner: Vec2,
     view_size: Vec2,
     pixel_colors: Vec<Color>,
-    pixel_cords: (Vec<f32>, Vec<f32>),
+    pixel_cords: (Vec<f64>, Vec<f64>),
 }
 impl MandelbrotRender {
     pub fn new(width: usize, height: usize, top_left_corner: Vec2, width_heigth: Vec2) -> Self {
@@ -30,19 +30,19 @@ impl MandelbrotRender {
             for y in 0..self.height {
                 let index = y * self.width + x;
                 let cords = self.top_left_corner + Vec2::new(x as f64, y as f64) * delta_pixel;
-                self.pixel_cords.0[index] = cords.x as f32;
-                self.pixel_cords.1[index] = cords.y as f32;
+                self.pixel_cords.0[index] = cords.x;
+                self.pixel_cords.1[index] = cords.y;
             }
         }
     }
     pub fn update_pixels(&mut self) {
         self.pixel_colors
-            .array_chunks_mut::<8>()
+            .array_chunks_mut::<4>()
             .zip(
                 self.pixel_cords
                     .0
-                    .array_chunks::<8>()
-                    .zip(self.pixel_cords.1.array_chunks::<8>()),
+                    .array_chunks::<4>()
+                    .zip(self.pixel_cords.1.array_chunks::<4>()),
             )
             .par_bridge()
             .for_each(|(pixel_colors, (x, y))| {
@@ -64,9 +64,9 @@ impl MandelbrotRender {
         }
     }
 }
-fn get_color(iterations: [i32; 8]) -> [Color; 8] {
-    let mut colors = [BLACK; 8];
-    for i in 0..8 {
+fn get_color(iterations: [i64; 4]) -> [Color; 4] {
+    let mut colors = [BLACK; 4];
+    for i in 0..4 {
         colors[i] = if iterations[i] != 129 {
             let r = (iterations[i] as f32 * 0.5).sin() * 0.5 + 0.5;
             let g = (iterations[i] as f32 * 0.5).cos() * 0.5 + 0.5;
@@ -79,24 +79,24 @@ fn get_color(iterations: [i32; 8]) -> [Color; 8] {
     colors
 }
 
-fn iterat_points(x: [f32; 8], y: [f32; 8]) -> [i32; 8] {
+fn iterat_points(x: [f64; 4], y: [f64; 4]) -> [i64; 4] {
     let mut iterator = MultiMandelIterator::new(x, y);
     iterator.iterate()
 }
 
 pub struct MultiMandelIterator {
-    z_x: f32x8,
-    z_y: f32x8,
-    z_squared_x: f32x8,
-    z_squared_y: f32x8,
-    c_x: f32x8,
-    c_y: f32x8,
-    iteration: i32x8,
+    z_x: f64x4,
+    z_y: f64x4,
+    z_squared_x: f64x4,
+    z_squared_y: f64x4,
+    c_x: f64x4,
+    c_y: f64x4,
+    iteration: i64x4,
 }
 impl MultiMandelIterator {
-    pub fn new(x: [f32; 8], y: [f32; 8]) -> Self {
-        let z_x = f32x8::from_array(x);
-        let z_y = f32x8::from_array(y);
+    pub fn new(x: [f64; 4], y: [f64; 4]) -> Self {
+        let z_x = f64x4::from_array(x);
+        let z_y = f64x4::from_array(y);
         Self {
             z_x,
             z_y,
@@ -104,7 +104,7 @@ impl MultiMandelIterator {
             z_squared_y: z_y * z_y,
             c_x: z_x,
             c_y: z_y,
-            iteration: i32x8::splat(1),
+            iteration: i64x4::splat(1),
         }
     }
     #[inline(always)]
@@ -114,13 +114,13 @@ impl MultiMandelIterator {
         self.z_squared_x = self.z_x * self.z_x;
         self.z_squared_y = self.z_y * self.z_y;
         let abs = self.z_squared_x + self.z_squared_y;
-        let new_iterations = abs.lanes_le(f32x8::splat(4.)).to_int();
+        let new_iterations = abs.lanes_le(f64x4::splat(4.)).to_int();
         self.iteration -= new_iterations; // lane is -1 when abs < 4
     }
-    fn get_iterations(&self) -> [i32; 8] {
+    fn get_iterations(&self) -> [i64; 4] {
         self.iteration.to_array()
     }
-    fn iterate(&mut self) -> [i32; 8] {
+    fn iterate(&mut self) -> [i64; 4] {
         for _ in 0..128 {
             self.next_iteration();
         }
