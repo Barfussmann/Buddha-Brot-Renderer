8f1777744a90e1e641d1c1633c7f65e300263854
Simon Thiesen <simon.thiesen@online.de>
2021-12-06 07:59:32 +0100

    added samples per cell

--- src/worker.rs
+++ src/worker.rs
@@ -1,9 +1,8 @@
+use super::cell::*;
+use super::util::four_point_inside_tests;
+use rand::prelude::{thread_rng, ThreadRng};
 use spmc;
 use std::sync::mpsc;
-use super::cell::*;
-use rand::prelude::{ThreadRng, thread_rng};
-use super::util::four_point_inside_tests;
-
 
 pub struct Worker {
     cell_to_sample: spmc::Receiver<[Cell; 4]>,
@@ -14,7 +13,13 @@ pub struct Worker {
     rng: ThreadRng,
 }
 impl Worker {
-    pub fn new(cell_to_work_on: spmc::Receiver<[Cell;4]>, cell_that_are_inside: mpsc::Sender<Cell>, limit: usize, sampels: usize, grid_size: usize){
+    pub fn new(
+        cell_to_work_on: spmc::Receiver<[Cell; 4]>,
+        cell_that_are_inside: mpsc::Sender<Cell>,
+        limit: usize,
+        sampels: usize,
+        grid_size: usize,
+    ) {
         Self {
             cell_to_sample: cell_to_work_on,
             cell_that_are_inside,
@@ -22,20 +27,25 @@ impl Worker {
             grid_size,
             limit,
             rng: thread_rng(),
-        }.work()
+        }
+        .work()
     }
     pub fn work(&mut self) {
         loop {
             let cells_result = self.cell_to_sample.recv();
-            if cells_result.is_err() {
-                return;
-            }
             let cells = cells_result.unwrap();
             let mut are_any_inside = [false; 4];
             for _ in 0..self.sampels {
-                let are_these_inside  = four_point_inside_tests(cells, self.limit, self.grid_size, &mut self.rng);
-                for (is_any_inside, is_this_inside) in std::iter::zip(&mut are_any_inside, are_these_inside) {
+                let are_these_inside = unsafe {
+                    four_point_inside_tests(cells, self.limit, self.grid_size, &mut self.rng)
+                };
+                for (is_any_inside, is_this_inside) in
+                    std::iter::zip(&mut are_any_inside, are_these_inside)
+                {
                     *is_any_inside |= is_this_inside;
+                }
+                if are_any_inside.iter().all(|a|*a) {
+                    break;
                 }
             }
             for (cell, is_inside) in std::iter::zip(cells, are_any_inside) {
@@ -43,8 +53,6 @@ impl Worker {
                     self.cell_that_are_inside.send(cell).unwrap();
                 }
             }
-            
-
         }
     }
 }
