7a855468733cd90c7d06e53acddf31d070d02a4e
Simon Thiesen <simon.thiesen@online.de>
2021-12-11 17:30:10 +0100

    removed dead code

--- src/covarage_grid/grid.rs
+++ src/covarage_grid/grid.rs
@@ -31,23 +31,6 @@ mod tests {
     use glam::ivec2;
     const GRID_SIZE: usize = 100;
     #[test]
-    // fn test_grid_new() {
-    //     let grid = Grid::new(GRID_SIZE);
-    //     assert_eq!(grid.collums.len(), GRID_SIZE);
-    // }
-    // #[test]
-    // fn new_grid_collums_empty() {
-    //     let grid = Grid::new(GRID_SIZE);
-    //     for collum in grid.collums.iter() {
-    //         assert!(collum.is_empty());
-    //     }
-    // }
-    // #[test]
-    // fn test_grid_insert() {
-    //     let mut grid = Grid::new(GRID_SIZE);
-    //     grid.insert(Cell::new(ivec2(10, 5)));
-    //     assert!(grid.collums[GRID_SIZE / 2 + 11].is_activ(GRID_SIZE / 2 + 5));
-    // }
     #[test]
     fn cell_activ_afte_insertion() {
         let mut grid = Grid::new(GRID_SIZE);
