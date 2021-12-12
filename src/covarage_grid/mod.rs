f3c3eb9927d4dac05c7b510cc57d8173fb67d968
Simon Thiesen <simon.thiesen@online.de>
2021-12-12 18:42:46 +0100

    added chaching

--- src/covarage_grid/mod.rs
+++ src/covarage_grid/mod.rs
@@ -6,20 +6,67 @@ mod sampled_cell;
 mod worker;
 
 use super::camera;
+use super::mandel_brot_render::MandelbrotRender;
 use super::mandel_iter;
 
+use covarage_grid_gen::CovarageGridGen;
+use macroquad::prelude::next_frame;
+use sample_cells::*;
+use std::fs;
 
 pub struct CovarageGrid {
     cells: Vec<cell::Cell>,
     size: usize,
 }
 impl CovarageGrid {
-    pub fn new(size: usize, cells: Vec<cell::Cell>) -> Self {
-        Self { cells, size }
+    pub async fn get_covarag_grid(
+        size: usize,
+        limit: usize,
+        samples_per_cell: usize,
+        sample_limit: usize,
+    ) -> CovarageGrid {
+        let file_name = CovarageGrid::get_file_name(size, limit, samples_per_cell);
+        let sample_cells: SampleCells = if let Ok(sampled_cells_data) = fs::read(file_name.clone())
+        {
+            bincode::deserialize(&sampled_cells_data).unwrap()
+        } else {
+            let sample_cells = CovarageGrid::gen_sample_cells(size, limit, samples_per_cell).await;
+            let data = bincode::serialize(&sample_cells).unwrap();
+            fs::write(file_name, &data).unwrap();
+            sample_cells
+        };
+        let cells = sample_cells.to_cells(sample_limit);
+
+        CovarageGrid { cells, size }
+    }
+    pub async fn gen_sample_cells(size: usize, limit: usize, samples_per_cell: usize) -> SampleCells {
+        let mut covarage_grid_gen = CovarageGridGen::new(limit, samples_per_cell, size);
+        let mut camera = camera::CameraManger::new();
+        let mut mandel_brot_render = MandelbrotRender::new();
+        while !covarage_grid_gen.is_finished() {
+            camera.update();
+            covarage_grid_gen.sample_neighbors();
+            mandel_brot_render.draw(&camera);
+            covarage_grid_gen.draw(&camera);
+
+            next_frame().await;
+        }
+        covarage_grid_gen.to_complet_sampled_cells()
     }
     pub fn draw(&self, camera: &camera::CameraManger) {
+        if !camera.draw_cells() {
+            return;
+        }
         for cell in self.cells.iter() {
             cell.draw(self.size, camera);
         }
+    }
+    fn get_file_name(size: usize, limit: usize, samples_per_cell: usize) -> String {
+        format!(
+            "./gridsize: {}, limit: {}, samples_per_cells: {}.cells",
+            size.to_string(),
+            limit.to_string(),
+            samples_per_cell.to_string(),
+        )
     }
 }
--- src/covarage_grid/sample_cells.rs
+++ src/covarage_grid/sample_cells.rs
@@ -1,31 +1,20 @@
-use super::{sampled_cell::*, CovarageGrid};
+use super::{cell::Cell, sampled_cell::*};
 use serde::{Deserialize, Serialize};
-use std::fs;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SampleCells {
-    cells: Vec<SampledCell>,
-    size: usize,
+    pub cells: Vec<SampledCell>,
+    pub size: usize,
 }
 impl SampleCells {
     pub fn new(cells: Vec<SampledCell>, size: usize) -> Self {
         SampleCells { cells, size }
     }
-    pub fn save(&self) {
-        let encoded = bincode::serialize(&self).unwrap();
-        fs::write("./sampeld_cells", encoded).unwrap();
-    }
-    pub fn load(&self) -> Self {
-        let data = fs::read("./sampled_cells").unwrap();
-        bincode::deserialize(&data).unwrap()
-    }
-    pub fn to_covarage_grid(&self, limit: usize) -> CovarageGrid {
-        let cells = self
-            .cells
+    pub fn to_cells(&self, limit: usize) -> Vec<Cell> {
+        self.cells
             .iter()
             .take_while(|a| a.get_highest_iteration() as usize >= limit)
             .map(|a| a.get_cell(self.size))
-            .collect();
-        CovarageGrid::new(self.size, cells)
+            .collect()
     }
 }
