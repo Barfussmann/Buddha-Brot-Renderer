595dbc0b40f08471b17c19474b237d5632fbfd72
Simon Thiesen <simon.thiesen@online.de>
2021-12-25 15:42:46 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod camera;
 mod covarage_grid;
 mod mandel_brot_render;
 mod mandel_iter;
+mod pixels;
 mod sample_gen;
 mod sample_mutator;
 
@@ -17,9 +18,10 @@ use lazy_static::*;
 
 const SIZE: usize = 1024;
 pub const WIDTH: usize = SIZE;
-pub const HEIGHT: usize = (SIZE as f64 * (2.64 / 3.0)) as usize;
+pub const HEIGHT: usize = SIZE;
+// pub const HEIGHT: usize = (SIZE as f64 * (2.64 / 3.0)) as usize;
 lazy_static! {
-    static ref COVARAGE_GRID: CovarageGrid = CovarageGrid::get_covarag_grid(10_000, 30, 10_000, 30);
+    static ref COVARAGE_GRID: CovarageGrid = CovarageGrid::get_covarag_grid(10_000, 50, 100_000, 50);
 }
 
 fn main() {
